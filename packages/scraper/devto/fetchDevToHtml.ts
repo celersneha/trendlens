01e1715e871347ae79439a8ba730e7bceca18266
Sneha Sharma <snehav2109@gmail.com>
2026-03-04 22:27:05 +0530

    Add executablePath to Puppeteer launch options for consistency

--- packages/scraper/devto/fetchDevToHtml.ts
+++ packages/scraper/devto/fetchDevToHtml.ts
@@ -4,6 +4,7 @@ import fs from "fs";
 export async function fetchDevToHtml() {
   const browser = await puppeteer.launch({
     headless: true,
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
